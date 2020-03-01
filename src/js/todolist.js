d7904f8ef5d62e1d6c73c382a722b1aab1766608
gibaek Park <yaqqww@nate.com>
2020-03-01 23:42:36 +0900

    todo 클릭 시 Checked class 토글

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -7,6 +7,17 @@ const todolist = {
 
     addBtn.addEventListener('click', addFunc);
     inputField.addEventListener('keyup', e => e.keyCode === 13 && addFunc());
+  },
+
+  todos() {
+    const myUl = document.querySelector('#myUL');
+    
+    myUl.addEventListener('click', e => {
+      const target = e.target;
+      if(target.nodeType === 1 && target.tagName === 'LI') {
+        target.classList.toggle('checked');
+      }
+    })
   }
 }
 
